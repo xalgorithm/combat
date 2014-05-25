a30c8586b47266d843c2f251cd0f1c821f8ac02a
Vincent Flesouras <x.algorithm@gmail.com>
2014-05-25 05:53:10 +0000

    Edits to profile system and updates to CSS

--- sites/all/themes/ursus/templates/node--event--teaser.tpl.php
+++ sites/all/themes/ursus/templates/node--event--teaser.tpl.php
@@ -31,9 +31,17 @@ if ($add_date_html_prefix) {
 }
 ?>
 
+<?php
+  $event_url = (isset($event_link) && $event_link != '') ? $event_link : $node_url;
+?>
+
+<?php
+  $ics = '/event/' . $nid . '/calendar.ics';
+?>
+
 <div class="event contextual-links-region">
-  <a href="<?php print $node_url; ?>"><?php print $title; ?></a> - <?php print $period; ?><?php print $location; ?>
-  <a class="icon" href="<?php print $node_url; ?>"></a>
+  <a href="<?php print $event_url; ?>"><?php print $title; ?></a> - <?php print $period; ?><?php print $location; ?>
+  <a class="icon" href="<?php print $ics; ?>"></a>
   <?php print render($title_suffix); ?>
 </div>
 
